067ef6cfc44b5123fad9b78acd44952c75a0f037
ninomae735 <66416228+ninomae735@users.noreply.github.com>
2020-06-04 22:01:05 +0900

    テスト

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,10 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def test(ctx):
+    await ctx.send('テスト')
 
+    
+    
 bot.run(token)
